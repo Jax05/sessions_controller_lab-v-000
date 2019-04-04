59a2032cbf8a657d45e8f7d4cb0a18608ad8c74b
Carley Tripp <carley.tripp@outlook.com>
2019-04-04 20:09:09 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
+    if session[:username].present?
+      session[:username] = params[:username]
+      redirect_to "/"
+    else
+      render :new
+    end
   end
 
   def destroy
