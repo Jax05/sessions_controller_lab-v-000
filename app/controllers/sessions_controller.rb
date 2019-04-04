13c9434e28df58b27b41e9e5c40e4b5b74eb7a54
Carley Tripp <carley.tripp@outlook.com>
2019-04-04 20:16:40 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
+    raise params.inspect
     if session[:name].present?
       session[:name] = params[:name]
       redirect_to "/"
